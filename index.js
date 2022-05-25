2a46e52544e7a1016c3725ea4b109ec14e481085
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-05-25 17:17:37 -0300

    POST, PUT, DELETE

--- index.js
+++ index.js
@@ -7,20 +7,44 @@ app.use("/meusite", express.static(path.join(__dirname, 'client')))
 
 app.get("/", (req,res) => {
 
-//  res.set("content-Type", "text/html")
-
-//  res.type('.html');
-//  res.type('html');
-//  res.type('json');
-//  res.type('application/json');
-//  res.type('png');
-
-    res.type("html")
     res.send("<h1>Hello World From GET</h1>")
 })
+
+app.post("/",(req,res) => {
+    
+        res.send("<h1>Hello World From POST</h1>")
+    })
+app.put("/",(req,res) => {
+    
+        res.send("<h1>Hello World From PUT</h1>")
+    })
+app.delete("/",(req,res) => {
+    
+        res.send("<h1>Hello World From DELETE</h1>")
+    })
+
 
 const PORT = 5000
 
 app.listen(PORT,() => { 
     console.log(`Servidor Rodando na porta ${PORT}`)
 }) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  res.type('.html');
+//  res.type('html');
+//  res.type('json');
+//  res.type('application/json');
+//  res.type('png');
